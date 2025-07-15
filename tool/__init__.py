123951971324ebe673cee0eff90bba9fb593f942
SupCodeTech <111235455+SupCodeTech@users.noreply.github.com>
2025-07-15 21:14:53 +0800

    Update __init__.py

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,1 +1,8 @@
-
+from .classification_cam_affinity import (
+    ClassificationHead,
+    CAMGenerator,
+    generate_initial_pseudo_label,
+    AffinityCalculator,
+    generate_affinity_labels,
+    compute_affinity_loss,
+)
